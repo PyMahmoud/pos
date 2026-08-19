929981bbf747e2855cbf8bd56b72d7b8f875d0ed
PyMahmoud <mahmoudo7m6677@gmail.com>
2026-08-19 17:19:15 +0300

    pos

--- PosSystem/PosSystem/obj/Debug/net10.0-windows/PosSystem_o431xbi3_wpftmp.AssemblyInfo.cs
+++ PosSystem/PosSystem/obj/Debug/net10.0-windows/PosSystem_o431xbi3_wpftmp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PosSystem")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0103474ce64beda2d3c672d4eff267627ded9df0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PosSystem")]
